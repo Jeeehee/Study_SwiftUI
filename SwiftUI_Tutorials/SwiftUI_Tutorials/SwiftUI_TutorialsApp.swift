f31e708fd5a7244a0272aa5a674022d643988798
jiheeHwang <hwangjihee94@gmail.com>
2024-07-02 13:12:41 +0900

    chore: SwiftUI Tutorials 프로젝트 생성

---
+++ SwiftUI_Tutorials/SwiftUI_Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Jihee hwang on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
